ff398f83a7966381ecf341bb7ef63b911300b07f
Izanagi078 <kashyaparman15@gmail.com>
2025-05-24 22:48:19 +0530

    Fixer

--- Backend/uploads/testArgon2.js
+++ Backend/uploads/testArgon2.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MzFkNDQ5Zjc5ZTY0Nzg5YmE1ZTQ1MiIsImlhdCI6MTc0ODA5NjA5NiwiZXhwIjoxNzQ4MDk5Njk2fQ.-xSy-AIjq5JFsAqYhqMDUGRGBVYECJD0vg6WjkAX2S8";
 const secret = process.env.JWT_SECRET;
 
 try {
