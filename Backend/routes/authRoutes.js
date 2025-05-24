a183f02280df85bebc0626814cd323b5607b62d1
Izanagi078 <kashyaparman15@gmail.com>
2025-05-24 22:44:53 +0530

    Pages

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const {protect} = require("../middleware/authMiddleware");
 const {
   registerUser,
   loginUser,
@@ -9,6 +10,6 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-// router.get("/getUser", protect, getUserInfo);
+router.get("/getUser", protect, getUserInfo);
 
 module.exports = router;
